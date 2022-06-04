3a761956fce5ac23beb3c075616b79ce22d10b49
KrisAesoey <kris.aesoey@gmail.com>
2022-06-04 18:48:22 +0200

    Add link to unique album page

--- src/components/album-chart/AlbumChartElement.tsx
+++ src/components/album-chart/AlbumChartElement.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Album } from '../../types/Types';
 import { GenreTagTextColorer, GenreTagBackgroundColorer } from '../../utils/TagColoring';
 import './albumChartElement.css';
@@ -10,29 +11,31 @@ import './albumChartElement.css';
  */
 const AlbumChartElement = ({ album } : { album : Album }) => {
     return (
-        <button className="albumButtonChart">
-            <div className="albumChart">
-                <div>
-                    <img src={album.image_url} alt="./rumours_fleetwoodmac.jpg" className="albumCoverChart"/>
+        <Link to={`/albums/${album.id}`}>
+            <button className="albumButtonChart">
+                <div className="albumChart">
+                    <div>
+                        <img src={album.image_url} alt="./rumours_fleetwoodmac.jpg" className="albumCoverChart"/>
+                    </div>
+                    <div>
+                        <b>{album.title}</b>
+                    </div>
+                    <div className="albumArtist">
+                        {album.artist}
+                    </div>
+                    <div>
+                        {album.genres.map((genre) =>
+                            <span key={genre} className="genreTagChart" style={{color: GenreTagTextColorer(genre), background: GenreTagBackgroundColorer(genre)}}>
+                                {genre}
+                            </span>
+                        )}
+                    </div>
+                    <div className="albumRating">
+                        <h1>{album.rating}</h1>
+                    </div>
                 </div>
-                <div>
-                    <b>{album.title}</b>
-                </div>
-                <div className="albumArtist">
-                    {album.artist}
-                </div>
-                <div>
-                    {album.genres.map((genre) =>
-                        <span key={genre} className="genreTagChart" style={{color: GenreTagTextColorer(genre), background: GenreTagBackgroundColorer(genre)}}>
-                            {genre}
-                        </span>
-                    )}
-                </div>
-                <div className="albumRating">
-                    <h1>{album.rating}</h1>
-                </div>
-            </div>
-        </button>
+            </button>
+        </Link>
     )
 };
 
